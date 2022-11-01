abf5139a4b1c364939ad7d99bb330e9974aef260
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-10-31 21:47:00 -0300

    :building_construction: build: initial setup

---
+++ src/main/java/com/example/vehicleinsurestoragesql/VehicleInsureStorageSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.vehicleinsurestoragesql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleInsureStorageSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleInsureStorageSqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vehicleinsurestoragesql/VehicleInsureStorageSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vehicleinsurestoragesql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleInsureStorageSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
