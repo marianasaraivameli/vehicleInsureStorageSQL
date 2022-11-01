983e3c9045bfbf5801fba02255ee5bd3e7219e20
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-10-31 21:50:00 -0300

    :rocket: feat: create Vehicle class in layer Model

---
+++ src/main/java/com/example/vehicleinsurestoragesql/model/Vehicle.java
@@ -0,0 +1,31 @@
+package com.example.vehicleinsurestoragesql.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String plate;
+    private String brand;
+    private String model;
+    private Date yearOfProduction;
+    private Integer numberOfWheels;
+
+    // um veiculo - varios sinistros
+    @ManyToOne
+    @JoinColumn(name = "id_accident")
+    @JsonIgnoreProperties("vehicles")
+    private Accident accident;
+}
