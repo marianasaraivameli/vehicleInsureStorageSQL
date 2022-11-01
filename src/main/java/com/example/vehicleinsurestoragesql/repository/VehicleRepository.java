32d8a0960599b98e19f274f411f1a4015859119c
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-11-01 12:47:47 -0300

    :rocket: feat: implements method findAllPlates in layer repository

--- src/main/java/com/example/vehicleinsurestoragesql/repository/VehicleRepository.java
+++ src/main/java/com/example/vehicleinsurestoragesql/repository/VehicleRepository.java
@@ -2,6 +2,11 @@ package com.example.vehicleinsurestoragesql.repository;
 
 import com.example.vehicleinsurestoragesql.model.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+    @Query("select plate from Vehicle")
+    public List<String> findAllPlates();
 }
