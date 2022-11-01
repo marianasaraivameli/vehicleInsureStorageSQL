8fe01f44403d4829832ce6b78503c5892c719347
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-11-01 17:45:31 -0300

    :rocket: feat: implements method findVehicleByYearOfProductionEqualsAndNumberOfWheelsEquals in layer repository

--- src/main/java/com/example/vehicleinsurestoragesql/repository/VehicleRepository.java
+++ src/main/java/com/example/vehicleinsurestoragesql/repository/VehicleRepository.java
@@ -1,6 +1,7 @@
 package com.example.vehicleinsurestoragesql.repository;
 
 import com.example.vehicleinsurestoragesql.dto.VehiclePMDTO;
+import com.example.vehicleinsurestoragesql.dto.VehicleYDTO;
 import com.example.vehicleinsurestoragesql.model.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,4 +13,6 @@ public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
     public List<String> findAllPlates();
 
     public List<VehiclePMDTO> findVehicleByYearOfProductionEquals(Integer year);
+
+    public List<VehicleYDTO> findVehicleByYearOfProductionEqualsAndNumberOfWheelsEquals(Integer year, Integer doors);
 }
